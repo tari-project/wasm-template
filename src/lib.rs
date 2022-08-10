aebbe5da8a290e6d2690ea6931d3b47f82dd7df1
stringhandler <mikethetike@tari.com>
2022-08-10 16:49:36 +0200

    chore: init

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use tari_template_macros::template;
+
+#[template]
+mod counter {
+    struct Counter {
+        value: u32,
+    }
+
+    impl Counter {
+        pub fn new() -> Self {
+            Self { value: 0 }
+        }
+
+        pub fn value(&self) -> u32 {
+            self.value
+        }
+
+        pub fn increase(&mut self) {
+            self.value += 1;
+        }
+    }
+}
