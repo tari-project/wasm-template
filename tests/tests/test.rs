2ce447468a2f3a274ef6d27f5a3532249336386f
stringhandler <mikethetike@tari.com>
2022-10-05 19:11:49 +0200

    feat: increase test

--- tests/tests/test.rs
+++ tests/tests/test.rs
@@ -2,11 +2,20 @@
 mod test {
 
     use tari_dan_engine::tooling::TemplateTest;
+    use tari_template_lib::args;
+    use tari_template_lib::models::ComponentAddress;
 
     #[test]
     fn test_increment() {
         let template_test = TemplateTest::new(vec!["../package"]);
-        // let store = template_test.state_store();
-        todo!()
+        let component_address: ComponentAddress =
+            template_test.call_function("Counter", "new", args![]);
+        let value: u32 = template_test.call_method(component_address, "value", args![]);
+
+        assert_eq!(value, 0);
+
+        template_test.call_method::<()>(component_address, "increase", args![]);
+        let value: u32 = template_test.call_method(component_address, "value", args![]);
+        assert_eq!(value, 1);
     }
 }
