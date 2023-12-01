3b39e018b72d31cf129e24ee3537af3ad17fa1ba
stringhandler <mikethetike@tari.com>
2023-12-01 11:41:52 +0200

    fix: update to latest template_lib

--- fungible/package/src/lib.rs
+++ fungible/package/src/lib.rs
@@ -9,15 +9,17 @@ mod faucet_template {
     }
 
     impl TestFaucet {
-        pub fn mint(initial_supply: Amount, token_symbol: String) -> Self {
+        pub fn mint(initial_supply: Amount, token_symbol: String) {
             let coins = ResourceBuilder::fungible()
                 .with_token_symbol(&token_symbol)
                 .initial_supply(initial_supply)
                 .build_bucket();
 
-            Self {
+            Component::new(Self {
                 vault: Vault::from_bucket(coins),
-            }
+            })
+            .with_access_rules(AccessRules::allow_all())
+            .create();
         }
 
         pub fn take_free_coins(&mut self) -> Bucket {
